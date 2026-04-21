2694e4e8405c9c81f078fa9b544c57e713bedc08
junojoyce685-hue <junojoyce685@gmail.com>
2026-04-22 00:09:04 +0530

    added event_planner and Footer component; fixes build scripts

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Footer = () => {
+    return(<footer>
+<section>
+    <p>&copy; 2024 Event Planner Organization. All rights reserved.</p>
+  </section>
+</footer>);};
+export default Footer;
